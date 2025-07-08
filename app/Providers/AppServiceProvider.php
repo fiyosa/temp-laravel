6798becb746bd5effaa27705623a5f4e2add2162
fiyosa <fiyosa97@gmail.com>
2025-07-08 16:58:51 +0700

    edit: minor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,11 +18,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(UrlGenerator $url): void
+    public function boot(): void
     {
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
